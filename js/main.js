dafe3b23f7bb697d2b0d389c68be8601821fae82
VladislavMungin <93925061+VladislavMungin@users.noreply.github.com>
2023-04-17 09:16:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,14 +13,7 @@ document.getElementById('main-action').onclick = function(){
     document.getElementById('cars').scrollIntoView({behavior:"smooth"});
 }
 
-var buttons = document.getElementsByClassName('car-button');
 
-for(var i = 0;i<buttons.length;i++){
-    buttons[i].onclick = function(){
-        document.getElementById('price').scrollIntoView({behavior:"smooth"});
-     
-    }
-}
 
 document.getElementById('price-button').onclick = function(){
     if(document.getElementById('name').value ===''){
